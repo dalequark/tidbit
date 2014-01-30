4fe3ab2246a7ff1b953ae07f032ee743ae4c5aad
Dale <damarkowitz@gmail.com>
2014-01-30 18:01:59 -0500

    ??????

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,21 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
 
+  before_create :create_remember_token
+  before_save {self.email = email.downcase}
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
+
 end
